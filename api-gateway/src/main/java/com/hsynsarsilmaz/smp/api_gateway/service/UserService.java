6bfab2d382f8c44ae6200e8a465eee58df335de1
Hüseyin Sarsılmaz <huseyinsarsilmaz@hotmail.com>
2025-06-01 15:43:10 +0300

    Add javadoc style comments in user service interface

--- api-gateway/src/main/java/com/hsynsarsilmaz/smp/api_gateway/service/UserService.java
+++ api-gateway/src/main/java/com/hsynsarsilmaz/smp/api_gateway/service/UserService.java
@@ -4,5 +4,12 @@ import com.hsynsarsilmaz.smp.api_gateway.model.entity.User;
 
 public interface UserService {
 
+    /**
+     * Retrieves a user by their email address.
+     *
+     * @param email the email to search for
+     * @return the User object if found
+     * @throws RuntimeException if no user is found with the given email
+     */
     public User getByEmail(String email);
 }
