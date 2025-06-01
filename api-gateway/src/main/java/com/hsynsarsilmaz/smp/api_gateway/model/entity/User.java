dc96a22060d5b7b2e2f2b3fb86145c50258b89ac
Hüseyin Sarsılmaz <huseyinsarsilmaz@hotmail.com>
2025-06-01 15:21:13 +0300

    Add user entity

---
+++ api-gateway/src/main/java/com/hsynsarsilmaz/smp/api_gateway/model/entity/User.java
@@ -0,0 +1,48 @@
+package com.hsynsarsilmaz.smp.api_gateway.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@Table(name = "users")
+public class User extends SmpEntity {
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String roles;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String surname;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    public enum Role {
+        ROLE_USER,
+        ROLE_ADMIN
+    }
+
+    @Builder.Default
+    @Column(nullable = false)
+    private Boolean isActive = true;
+
+}
