5d9c5cc30037f12838136bffae9d0405f740483b
Hüseyin Sarsılmaz <huseyinsarsilmaz@hotmail.com>
2025-06-02 13:57:18 +0300

    Add register dto

--- api-gateway/src/main/java/com/hsynsarsilmaz/smp/api_gateway/model/dto/request/RegisterRequest.java
+++ api-gateway/src/main/java/com/hsynsarsilmaz/smp/api_gateway/model/dto/request/RegisterRequest.java
@@ -1,5 +1,43 @@
 package com.hsynsarsilmaz.smp.api_gateway.model.dto.request;
 
+import com.hsynsarsilmaz.smp.api_gateway.validation.Alphabethical;
+import com.hsynsarsilmaz.smp.api_gateway.validation.PhoneNumber;
+import com.hsynsarsilmaz.smp.api_gateway.validation.RequiredField;
+import com.hsynsarsilmaz.smp.api_gateway.validation.StrSize;
+
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class RegisterRequest {
+
+    @Email(message = "{fail.email.format}")
+    @RequiredField(entityName = "User", fieldName = "email")
+    private String email;
+
+    @RequiredField(entityName = "User", fieldName = "password")
+    @StrSize(entityName = "User", fieldName = "password", min = 8, max = 32)
+    private String password;
+
+    @RequiredField(entityName = "User", fieldName = "name")
+    @StrSize(entityName = "User", fieldName = "name", min = 2, max = 32)
+    @Alphabethical(entityName = "User", fieldName = "name")
+
+    private String name;
+
+    @RequiredField(entityName = "User", fieldName = "surname")
+    @StrSize(entityName = "User", fieldName = "surname", min = 2, max = 32)
+    @Alphabethical(entityName = "User", fieldName = "surname")
+    private String surname;
+
+    @RequiredField(entityName = "User", fieldName = "phone number")
+    @PhoneNumber(entityName = "User", fieldName = "phone number")
+    private String phoneNumber;
+
+    private String secretCode;
 
 }
