cdf6032016411fcca00bf46de290b7fdf797654d
Hüseyin Sarsılmaz <huseyinsarsilmaz@hotmail.com>
2025-06-02 14:18:19 +0300

    Add mapstruct

---
+++ api-gateway/src/main/java/com/hsynsarsilmaz/smp/api_gateway/model/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.hsynsarsilmaz.smp.api_gateway.model.mapper;
+
+import org.mapstruct.*;
+
+import com.hsynsarsilmaz.smp.api_gateway.model.dto.request.RegisterRequest;
+import com.hsynsarsilmaz.smp.api_gateway.model.entity.User;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+    @Mappings({
+            @Mapping(target = "isActive", ignore = true),
+            @Mapping(target = "role", ignore = true),
+    })
+    User toEntity(RegisterRequest req);
+}
