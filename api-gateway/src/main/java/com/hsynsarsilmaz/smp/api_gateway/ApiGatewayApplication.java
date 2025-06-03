cddb4a73235b4faf4502c3cc2471b9773636dcc7
Hüseyin Sarsılmaz <huseyinsarsilmaz@hotmail.com>
2025-06-03 19:10:02 +0300

    Add mcv gateway configs

--- api-gateway/src/main/java/com/hsynsarsilmaz/smp/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/hsynsarsilmaz/smp/api_gateway/ApiGatewayApplication.java
@@ -2,8 +2,13 @@ package com.hsynsarsilmaz.smp.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication(scanBasePackages = "com.hsynsarsilmaz.smp")
+@SpringBootApplication(scanBasePackages = {
+		"com.hsynsarsilmaz.smp.api_gateway",
+		"com.hsynsarsilmaz.smp.common"
+})
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
