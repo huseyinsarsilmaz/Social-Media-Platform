a17d2b5afef4bcc8d69f3c085b4cca31e4f82f83
Hüseyin Sarsılmaz <huseyinsarsilmaz@hotmail.com>
2025-06-08 16:36:34 +0300

    Fix hydration error

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,21 +1,25 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Providers } from "./providers";
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
+
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata: Metadata  = {
+export const metadata: Metadata = {
   title: 'Microblog App',
   description: 'Twitter clone with microservices',
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Providers>{children}</Providers>
-      </body>
-    </html>
-  );
-}
+
+ export default function RootLayout(props : any) {
+   return (
+     <html lang="en">
+       <body>
++        <AppRouterCacheProvider>
+           {props.children}
++        </AppRouterCacheProvider>
+       </body>
+     </html>
+   );
+ }
