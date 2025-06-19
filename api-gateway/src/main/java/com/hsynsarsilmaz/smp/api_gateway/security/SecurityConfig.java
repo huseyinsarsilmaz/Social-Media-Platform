5a09042b49d4194147f4fa280a6a5ad38008f972
Hüseyin Sarsılmaz <huseyinsarsilmaz@hotmail.com>
2025-06-19 15:28:43 +0300

    User env value for frontend url

--- api-gateway/src/main/java/com/hsynsarsilmaz/smp/api_gateway/security/SecurityConfig.java
+++ api-gateway/src/main/java/com/hsynsarsilmaz/smp/api_gateway/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.hsynsarsilmaz.smp.api_gateway.security;
 import java.io.IOException;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -35,6 +36,9 @@ public class SecurityConfig {
     private final JwtAuthFilter jwtAuthFilter;
     private final UserIdHeaderGatewayFilter userIdHeaderGatewayFilter;
 
+    @Value("${frontend.url}")
+    private String frontendUrl;
+
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
@@ -52,7 +56,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
             CorsConfiguration configuration = new CorsConfiguration();
-            configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+            configuration.setAllowedOrigins(List.of(frontendUrl));
             configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
             configuration.setAllowedHeaders(List.of("*"));
             configuration.setAllowCredentials(true);
